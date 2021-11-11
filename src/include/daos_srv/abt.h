1566e39ac6b45c262ddbda600fbb0ebeb089ae1e
Li Wei <wei.g.li@intel.com>
2021-11-11 16:24:49 +0800

    WIP
    Signed-off-by: Li Wei <wei.g.li@intel.com>

---
+++ src/include/daos_srv/abt.h
@@ -0,0 +1,24 @@
+/*
+ * (C) Copyright 2021 Intel Corporation.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause-Patent
+ */
+/**
+ * \file
+ *
+ * dss_abt: Argobots Helpers
+ */
+
+#ifndef DAOS_SRV_ABT_H
+#define DAOS_SRV_ABT_H
+
+#define DABT_EVENTUAL_SET(eventual, value, nbytes)						\
+	do {											\
+		int dabt_rc;									\
+												\
+		dabt_rc = ABT_eventual_set(eventual, value, nbytes);				\
+		D_ASSERTF(dabt_rc == ABT_SUCCESS, "ABT_eventual_set(%p, %d): %d\n", value,	\
+			  nbytes, dabt_rc);							\
+	} while (0)
+
+#endif /* DAOS_SRV_ABT_H */
