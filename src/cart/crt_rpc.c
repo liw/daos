89abbb11b3aa369857fa162d20fdbaaa54f48d64
Li Wei <wei.g.li@intel.com>
2021-11-15 15:37:53 +0800

    Reduce POOL_TGT_QUERY_MAP timeouts
    Signed-off-by: Li Wei <wei.g.li@intel.com>

--- src/cart/crt_rpc.c
+++ src/cart/crt_rpc.c
@@ -637,6 +637,24 @@ out:
 	return rc;
 }
 
+int
+crt_req_get_timeout(crt_rpc_t *req, uint32_t *timeout_sec)
+{
+	struct crt_rpc_priv	*rpc_priv;
+	int			 rc = 0;
+
+	if (req == NULL || timeout_sec == NULL) {
+		D_ERROR("invalid parameter (NULL req or timeout_sec).\n");
+		D_GOTO(out, rc = -DER_INVAL);
+	}
+
+	rpc_priv = container_of(req, struct crt_rpc_priv, crp_pub);
+	*timeout_sec = rpc_priv->crp_timeout_sec;
+
+out:
+	return rc;
+}
+
 /* Called from a decref() call when the count drops to zero */
 void
 crt_req_destroy(struct crt_rpc_priv *rpc_priv)
--- src/pool/cli.c
+++ src/pool/cli.c
@@ -1484,6 +1484,7 @@ create_map_refresh_rpc(struct dc_pool *pool, unsigned int map_version,
 	crt_rpc_t		       *c;
 	struct pool_tgt_query_map_in   *in;
 	struct pool_buf		       *b;
+	uint32_t			timeout;
 	int				rc;
 
 	ep.ep_grp = group;
@@ -1507,6 +1508,12 @@ create_map_refresh_rpc(struct dc_pool *pool, unsigned int map_version,
 		crt_req_decref(c);
 		return rc;
 	}
+
+	/* Set a timeout shorter than the default one. */
+	rc = crt_req_get_timeout(c, &timeout);
+	D_ASSERTF(rc = 0, "crt_req_get_timeout: "DF_RC"\n", DP_RC(rc));
+	rc = crt_req_set_timeout(c, max(1, timeout / 4));
+	D_ASSERTF(rc = 0, "crt_req_set_timeout: "DF_RC"\n", DP_RC(rc));
 
 	*rpc = c;
 	*map_buf = b;
